67fb5b7e1b27f0984a479559afe87a62fa5b0663
Jideotetic <jideotetic@gmail.com>
2025-07-28 08:26:13 +0100

    Updated bash script

--- setup-systemd.sh
+++ setup-systemd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SERVICE_NAME="stellar-rpc-node-instance-pubnet"
-APP_DIR="/home/jideotetic/stellar-rpc-node-instance-pubnet"
+APP_DIR="/home/tinkerpal/stellar-rpc-node-instance-pubnet"
 REPO_URL="git@github.com:Jideotetic/stellar-rpc-node-instance-pubnet.git"
 DOCKER_COMPOSE_BIN="/usr/bin/docker compose"
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
